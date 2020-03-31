2f0965bb8ab9d2c5693c6f7e47d36de36f1b9072
Nicolas <nicolas.carrasco.m@gmail.com>
2020-03-31 07:06:15 -0300

    first commit

---
+++ src/app/alineacion/alineacion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alineacion',
+  templateUrl: './alineacion.component.html',
+  styleUrls: ['./alineacion.component.css']
+})
+export class AlineacionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
